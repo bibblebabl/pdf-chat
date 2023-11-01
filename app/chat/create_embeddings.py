707add153f5f96c0350fefe5d30640a0de1c1faf
Igor Belogurov <belogurovigor@gmail.com>
2023-11-01 19:21:29 +0100

    build_retriever

--- app/chat/create_embeddings.py
+++ app/chat/create_embeddings.py
@@ -2,9 +2,11 @@ from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from app.chat.vector_stores.pinecone import vector_store
 
-
 def create_embeddings_for_pdf(pdf_id: str, pdf_path: str):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=100
+    )
 
     loader = PyPDFLoader(pdf_path)
     docs = loader.load_and_split(text_splitter)
@@ -12,8 +14,8 @@ def create_embeddings_for_pdf(pdf_id: str, pdf_path: str):
     for doc in docs:
         doc.metadata = {
             "page": doc.metadata["page"],
-            "pdf_id": pdf_id,
             "text": doc.page_content,
+            "pdf_id": pdf_id
         }
 
     vector_store.add_documents(docs)
