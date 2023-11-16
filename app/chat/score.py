ab30f60348b6c53e27ac78f493ecf8c10bd82133
Igor Belogurov <belogurovigor@gmail.com>
2023-11-16 21:46:50 +0100

    Refactored random component selection to use weighted averages

--- app/chat/score.py
+++ app/chat/score.py
@@ -10,12 +10,19 @@ def random_component_by_score(component_type, component_map):
     # From redis, get the hash containing the number of times each component has been voted on
     counts = client.hgetall(f"{component_type}_score_counts")
 
-    print(values, counts)
     # Get all the valid component names from the component map
+    names = component_map.keys()
 
     # Loop over those valid names and use them to calculate the average score for each
     # Add average score to a dictionary
+    avg_scores = {}
+    for name in names:
+        score = int(values.get(name, 1))
+        count = int(counts.get(name, 1))
+        avg = score / count
+        avg_scores[name] = max(avg, 0.1)
 
+    print(avg_scores)
     # Do a weighted random selection
 
 def score_conversation(
